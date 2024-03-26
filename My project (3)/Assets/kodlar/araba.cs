c24ca0a261caed3be295c78619cb6f3aaff656ec
fsocietyazec <fsocietyazec@gmail.com>
2024-03-26 19:03:13 +0300

    SLM

---
+++ My project (3)/Assets/kodlar/araba.cs
@@ -0,0 +1,19 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class araba : MonoBehaviour
+{
+    public WheelCollider önsoltekerlek, önsagtekerlek, arkasoltekerlek, arkasağtekerlek;
+
+    public float motorhızı,dönmehızı;
+    public Joystick tus;
+   
+    void Update()
+    {
+        arkasağtekerlek.motorTorque = motorhızı * tus.Vertical;
+        arkasoltekerlek.motorTorque = motorhızı * tus.Vertical;
+        önsagtekerlek.steerAngle = dönmehızı * tus.Horizontal;
+        önsoltekerlek.steerAngle = dönmehızı * tus.Horizontal;
+    }
+}
